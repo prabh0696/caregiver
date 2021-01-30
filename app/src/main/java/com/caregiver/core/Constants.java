32ad810748d94c315819f3b67d9397be85912ebd
Amandeep <amandeep5678kaur@gmail.com>
2021-01-30 18:01:03 -0500

    Updation of Login Option

--- app/src/main/java/com/caregiver/core/Constants.java
+++ app/src/main/java/com/caregiver/core/Constants.java
@@ -1,10 +1,36 @@
 package com.caregiver.core;
 
-public class Constants {
-    public static int USER_TYPE_ADMIN = 1,
-            USER_TYPE_GENERAL = 2,
-            USER_TYPE_NURSE = 3,
-            USER_TYPE_NANEY = 4;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 
-    public static String key_user_type = "user_type";
+import com.caregiver.database.DataBaseHelper;
+
+public class Constants {
+
+    public static int loginUserId = 0;
+
+    public static int USER_TYPE_ADMIN = 0,
+            USER_TYPE_GENERAL = 1,
+            USER_TYPE_NURSE = 2,
+            USER_TYPE_NANEY = 3;
+
+
+    public static String key_user_type = "user_type",
+            key_user = "user",
+            key_is_from_signup = "is_from_signup",
+            key_is_from_edit = "is_from_edit";
+
+    private static SQLiteDatabase _database;
+
+    public static String MOVE_TO_HOME_ACTION = "Move_to_home",
+            MOVE_TO_LOGIN_ACTION = "Move_to_login",
+            PROFILE_UPDATED_ACTION = "profile_updated";
+
+
+    public static SQLiteDatabase getDataBaseObj(Context context) {
+        if (_database == null || !_database.isOpen()) {
+            _database = DataBaseHelper.getInstance(context).getWritableDatabase();
+        }
+        return _database;
+    }
 }
--- app/src/main/java/com/caregiver/ui/login/LoginOptionActivity.java
+++ app/src/main/java/com/caregiver/ui/login/LoginOptionActivity.java
@@ -1,27 +1,33 @@
 package com.caregiver.ui.login;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.View;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.request.RequestOptions;
-import com.caregiver.MainActivity;
 import com.caregiver.R;
 import com.caregiver.core.Constants;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatImageView;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 public class LoginOptionActivity extends AppCompatActivity {
+
+    private BroadcastReceiver broadcastReceiver;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_option);
 
+        registerLocalBroadcastReciver();
         clickListener();
         setupViewPager();
 
@@ -59,8 +65,32 @@ public class LoginOptionActivity extends AppCompatActivity {
     }
 
     private void submitAction(int userType) {
-        Intent intent = new Intent(this, SignupActivity.class);
+        Intent intent = new Intent(this, LoginActivity.class);
         intent.putExtra(Constants.key_user_type, userType);
         startActivity(intent);
     }
+
+    private void registerLocalBroadcastReciver() {
+        broadcastReceiver = new BroadcastReceiver() {
+
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+                if (action.equalsIgnoreCase(Constants.MOVE_TO_HOME_ACTION)) {
+                    finish();
+                }
+            }
+        };
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(Constants.MOVE_TO_HOME_ACTION);
+
+        LocalBroadcastManager.getInstance(getApplicationContext()).registerReceiver(broadcastReceiver, intentFilter);
+
+    }
+
+    public void finish(){
+        LocalBroadcastManager.getInstance(getApplicationContext()).unregisterReceiver(broadcastReceiver);
+        super.finish();
+    }
+
 }
