243a174b27dcdf831b77c6780e8e42831ef9da8d
madhavmakkar <madhavmakkar11@gmail.com>
2021-02-14 15:10:24 -0500

    Booking Model Class

---
+++ app/src/main/java/com/caregiver/core/models/Booking.java
@@ -0,0 +1,23 @@
+package com.caregiver.core.models;
+
+public class Booking {
+    public String booking_id,
+            from_user_id,
+            to_user_id,
+            from_date,
+            to_date,
+            from_time,
+            to_time,
+            booking_status,
+            booked_date,
+            booking_message,
+            from_email,
+            from_phone,
+            from_first_name,
+            from_last_name,
+            to_email,
+            to_phone,
+            to_first_name,
+            to_last_name;
+
+}
