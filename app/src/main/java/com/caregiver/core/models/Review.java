6a00151045a0b45db525eb8c17bd02478b052782
Amandeep <amandeep5678kaur@gmail.com>
2021-02-22 19:51:38 -0500

    Review model class

---
+++ app/src/main/java/com/caregiver/core/models/Review.java
@@ -0,0 +1,18 @@
+package com.caregiver.core.models;
+
+public class Review {
+    public String review = "",
+            review_id = "",
+            from_user_id = "",
+            to_user_id = "",
+            booking_id = "",
+            review_date = "",
+            from_email = "",
+            from_phone = "",
+            from_first_name = "",
+            from_last_name = "",
+            to_email = "",
+            to_phone = "",
+            to_first_name = "",
+            to_last_name = "";
+}
